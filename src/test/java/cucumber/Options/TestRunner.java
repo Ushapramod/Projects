7f6141e82194e7e4938044c947eea3621646ca4e
Ushapramod <ushanischay2016@gmail.com>
2025-02-03 15:05:13 +0530

    untill product management

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -1,16 +1,15 @@
 package cucumber.Options;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/features", 
+        features = "src/test/resources/features",  // ✅ Correct path
         glue = "stepDefinitions", 
         tags = "@SortResults1",
-       plugin = {"pretty", "html:target/cucumber-reports/html", "json:target/cucumber-reports/cucumber.json"} 
+        plugin = {"pretty", "html:target/cucumber-reports/html", "json:target/cucumber-reports/cucumber.json"}
 )
 public class TestRunner {
     // Empty class - used to run Cucumber tests
